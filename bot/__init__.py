1f7ccadbb8059858f0605dfc07c7ae77fc8cf445
M. Fahmi Amaruddin <fahmi.amaruddin@students.amikom.ac.id>
2022-04-27 18:45:28 +0700

    try

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,13 +13,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 ENV = bool(os.environ.get('ENV', False))
 try:
   if ENV:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SUDO_USERS = os.environ.get('SUDO_USERS')
-    SUPPORT_CHAT_LINK = os.environ.get('SUPPORT_CHAT_LINK', 'https://t.me/ViperCommunity')
-    DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
+    BOT_TOKEN = "5325487977:AAF_bqTQHsCvg3GjzALHUPX8rqZAnOP6w1U"
+    APP_ID = 7024875
+    API_HASH = "bb2e5a71459ada0da536a8a7974fee39"
+    DATABASE_URL = "postgresql://gpyjzccm:8qz-LRFNVH8zv8PmsqW7lj322GnRDiUt@arjuna.db.elephantsql.com/gpyjzccm"
+    SUDO_USERS = 1993087643
+    SUPPORT_CHAT_LINK = 'https://t.me/ViperCommunity'
+    DOWNLOAD_DIRECTORY = "./downloads/"
   else:
     from bot.config import config
     BOT_TOKEN = config.BOT_TOKEN
@@ -30,7 +30,7 @@ try:
     SUPPORT_CHAT_LINK = config.SUPPORT_CHAT_LINK
     DOWNLOAD_DIRECTORY = config.DOWNLOAD_DIRECTORY
   SUDO_USERS = list(set(int(x) for x in SUDO_USERS.split()))
-  SUDO_USERS.append(939425014)
+  # SUDO_USERS.append(939425014)
   SUDO_USERS = list(set(SUDO_USERS))
 except KeyError:
   LOGGER.error('One or more configuration values are missing exiting now.')
